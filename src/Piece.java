0f25e5ba6f5df6def9cb8af9868b5057963e88f2
diallom224 <diallom1000@gmail.com>
2025-12-03 21:52:19 +0100

    fin de la class Piece

--- src/Piece.java
+++ src/Piece.java
@@ -1,8 +1,8 @@
 
 
-public class Piece {
-    String nom;
-    String reference;
+public abstract class Piece {
+    protected String nom;
+    protected String reference;
 
 
     //Le constructeur 
@@ -10,4 +10,28 @@ public class Piece {
         this.nom = nom;
         this.reference = reference;
     }
+
+   @Override
+    public String toString() {
+        return nom + " - " + reference;
+    }
+
+    protected void setReference(String reference){
+        this.reference = reference;
+    }
+
+
+    // mes classe abstract : 
+
+    public abstract void affiche();
+
+    public abstract double prix();
+
+    public abstract int dureeGarantie();
+
+    public abstract int dureeGarantieDeBase();
+
+    public abstract int dureefabrication();
+
+    
 }
