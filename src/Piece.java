96c1523551466260812a38df58085c778f0f6821
diallom224 <diallom1000@gmail.com>
2025-12-03 21:37:13 +0100

    ajout de de constructeur

--- src/Piece.java
+++ src/Piece.java
@@ -4,6 +4,8 @@ public class Piece {
     String nom;
     String reference;
 
+
+    //Le constructeur 
     public Piece(String nom,String reference){
         this.nom = nom;
         this.reference = reference;
