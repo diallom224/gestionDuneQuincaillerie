347bc2e96b0574346f44342a539afaef15cba8b5
diallom224 <diallom1000@gmail.com>
2025-12-03 21:22:37 +0100

    ajouter de la classe piece

---
+++ src/Piece.java
@@ -0,0 +1,11 @@
+
+
+public class Piece {
+    String nom;
+    String reference;
+
+    public Piece(String nom,String reference){
+        this.nom = nom;
+        this.reference = reference;
+    }
+}
